2dfcad359a48bc76f81effe12e5581923d650fad
BoringCrypto <b@rtje.net>
2021-05-05 19:49:46 +1200

    Bootstrap working

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
+<style lang="scss">
+@import "src/scss/main.scss";
+</style>
+
 <template>
-  <div>
+  <div class="text-center">
     <img alt="Dictator DAO Logo" src="./assets/logo.jpeg" height="180" />
     <h1>Dictator DAO</h1>
 
     <p>
-      Dictator DAO is an organization where the token holders democratically elect a 'dictator' to operate the protocol.
-      Whenever the DAO isn't happy, they can simply elect a new 'dictator'.
+      Dictator DAO is an organization where the token holders democratically
+      elect a 'dictator' to operate the protocol. Whenever the DAO isn't happy,
+      they can simply elect a new 'dictator'.
     </p>
     <Web3 :info="web3">
       <template v-slot:none>
@@ -14,40 +19,46 @@
       <template v-slot:connect>
         <button @click="web3.connect">Connect Metamask</button>
       </template>
-      Name: {{ web3.name }}<br>
-      Connected: {{ web3.connected }}<br>
-      Address: {{ web3.address }}<br>
-      ChainId: {{ web3.chainId }}<br>
-      Block: {{ web3.block }}
+      <table class="table mx-auto text-start" style="width: 600px">
+        <tr>
+          <td>Name</td>
+          <td>{{ web3.name }}</td>
+        </tr>
+        <tr>
+          <td>Connected</td>
+          <td>{{ web3.connected }}</td>
+        </tr>
+        <tr>
+          <td>Address</td>
+          <td>{{ web3.address }}</td>
+        </tr>
+        <tr>
+          <td>ChainId</td>
+          <td>{{ web3.chainId }}</td>
+        </tr>
+        <tr>
+          <td>Block</td>
+          <td>{{ web3.block }}</td>
+        </tr>
+      </table>
     </Web3>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, Ref, ref } from 'vue'
-import Web3, { EmptyProviderInfo, ProviderInfo } from './components/Web3.vue'
+import { defineComponent, Ref, ref } from "vue";
+import Web3, { EmptyProviderInfo, ProviderInfo } from "./components/Web3.vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    Web3
+    Web3,
   },
   setup: () => {
-    const web3: Ref<ProviderInfo> = ref(EmptyProviderInfo)
+    const web3: Ref<ProviderInfo> = ref(EmptyProviderInfo);
     return {
-      web3
-    }
-  }
-})
+      web3,
+    };
+  },
+});
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 20px;
-}
-</style>
